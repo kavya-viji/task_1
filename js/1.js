cfc1d709529e4dc0fbf89cd406976684ef297a23
Admin <kavyak6999@gmail.com>
2022-12-14 15:51:29 +0530

    divisibility by 7

---
+++ js/1.js
@@ -0,0 +1,20 @@
+var x = 49;
+if(x%7==0)
+{
+	alert ("The number is divisible by 7");
+
+if (x==49)
+ {
+	 alert ("The number is divisible by 49");
+ }
+
+else
+{
+	alert ("The number is not divisible by 49");
+}
+}
+else
+{
+	alert ("The number is not divisible by 7");
+}
+
